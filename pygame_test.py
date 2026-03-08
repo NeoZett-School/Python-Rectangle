60b9369e94b7e44d39b70c0779a0b43e9b59ff85
Neo Östlund Zetterberg <neo.ostlundzetterbe@learnet.se>
2026-03-08 14:18:58 +0100

    Made a pygame example.

---
+++ pygame_test.py
@@ -0,0 +1,38 @@
+from Rectangle import pygame_phys_premade as rtngle_phys_premade
+from Rectangle import Rect, Color
+import pygame
+import sys
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+WHITE = Color(255, 255, 255)
+ORANGE = Color(255, 125, 0)
+
+box = rtngle_phys_premade.Box(
+    surface = screen, 
+    rect = Rect(10, 10, 50, 50), 
+    color = ORANGE, 
+    use_detail = False # For performance, DO NOT USE the internal surface (Useful for complex usecases)
+)
+
+active = True
+while active:
+    dt = clock.tick(60.0) / 1000
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            active = False
+        box.handle_event(event)
+    pressed_keys = pygame.key.get_pressed()
+    screen.fill(WHITE)
+
+    box.update(pressed_keys, dt)
+    box.render()
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
